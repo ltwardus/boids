2b10e80f8606fda13778332b7bf41462099a0083
Łukasz Twarduś <ltwardus@gmail.com>
2016-04-12 19:01:36 +0200

    [] Initial commit, basic window displayed

---
+++ src/main.cc
@@ -0,0 +1,14 @@
+#include <SFML/Window.hpp>
+
+int main(int argc, char* argv[]) {
+  sf::Window window(sf::VideoMode(800, 600), "Boids");
+
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+    }
+  }
+};
